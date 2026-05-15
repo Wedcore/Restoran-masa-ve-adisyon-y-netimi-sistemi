2eb8d06d3a0fbc7f6b15ed3c93cfd7f44b90ed96
Wedcore X <sametyt051@gmail.com>
2026-05-15 19:16:29 +0300

    "Admin login sistemi ve iptal butonu eklendi"

--- Restoran masa ve adisyon yönetimi sistemi/Migrations/20260515154657_AdminKoddanKaldirildi.Designer.cs
+++ Restoran masa ve adisyon yönetimi sistemi/Migrations/20260515154657_AdminKoddanKaldirildi.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Restoran_masa_ve_adisyon_yönetimi_sistemi.Models;
 
@@ -11,9 +12,11 @@ using Restoran_masa_ve_adisyon_yönetimi_sistemi.Models;
 namespace Restoran_masa_ve_adisyon_yönetimi_sistemi.Migrations
 {
     [DbContext(typeof(RestoranContext))]
-    partial class RestoranContextModelSnapshot : ModelSnapshot
+    [Migration("20260515154657_AdminKoddanKaldirildi")]
+    partial class AdminKoddanKaldirildi
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Restoran masa ve adisyon yönetimi sistemi/Migrations/20260515154657_AdminKoddanKaldirildi.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restoran_masa_ve_adisyon_yönetimi_sistemi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminKoddanKaldirildi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Kullanicilar",
+                columns: new[] { "Id", "KullaniciAdi", "Rol", "Sifre" },
+                values: new object[] { 1, "admin", "Admin", "1234" });
+        }
+    }
+}
